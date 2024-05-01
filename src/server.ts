504700a84396f30af9d549f95adac1e792838e48
Jesse Emana <jesseemana@gmail.com>
2024-05-01 15:11:44 +0200

    initialized server

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import connectDb from './utils/database';
+import { UserRoute, OrderRoute, RestaurantRoute, MyRestaurantRoute, } from './routes';
+
+const app = express();
+const PORT = parseInt(process.env.PORT as string)|| 7000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', async (_req: Request, res: Response) => res.status(200).send('OK'));
+
+app.use('/api/order/checkout/webhook', express.raw({ type: '*/*' }));
+
+app.use('/api/order', OrderRoute);
+app.use('/api/my/user', UserRoute);
+app.use('/api/restaurant', RestaurantRoute);
+app.use('/api/my/restaurant', MyRestaurantRoute);
+
+app.listen(PORT, async () => {
+  await connectDb();
+  console.log('server started on http://localhost:7000');
+});
