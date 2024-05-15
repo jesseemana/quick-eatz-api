31c9e5154f62ca92885faa7e19bb102aad17ba11
Jesse Emana <jesseemana@gmail.com>
2024-05-15 15:28:30 +0200

    updated user schema

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,57 +1,57 @@
 import { Request, Response } from 'express';
+import { UserType } from '../schema/user.schema';
 import UserService from '../services/user.service';
-import { RegisterUserType, UpdateUserType } from '../schema/user.schema';
 
 
-const getCurrentUser = async (req: Request, res: Response) => {
+async function getCurrentUser(req: Request, res: Response) {
   try {
     const current_user = req.user;
     if (!current_user) return res.status(404).send('No user found.');
     return res.status(200).send(current_user);
   } catch (error) {
-    console.log('An error occured', error);
-    return res.status(500).send(`Internal Server Error.`);
+    console.log('An error occurred:', error);
+    return res.status(500).send(`Internal Server Error`);
   }
 };
 
 
-const registerUser = async (
-  req: Request<{}, {}, RegisterUserType>, 
+async function registerUser(
+  req: Request<{}, {}, UserType>, 
   res: Response
-) => {
+) {
   try {
-    const { auth0Id } = req.body;
+    const { auth0Id, email } = req.body;
     
     const existing_user = await UserService.findUser({ auth0Id });
     if (existing_user) return res.status(200).send();
     
-    const new_user = await UserService.createUser(req.body);
+    const new_user = await UserService.createUser({ auth0Id, email, });
 
     return res.status(201).json(new_user.toObject());
   } catch (error) {
-    console.log('An error occured', error);
-    return res.status(500).send(`Internal Server Error.`);
+    console.log('An error occurred:', error);
+    return res.status(500).send(`Internal Server Error`);
   }
 };
 
 
-const updateCurrentUser = async (
-  req: Request<{}, {}, UpdateUserType>, 
+async function updateCurrentUser(
+  req: Request<{}, {}, UserType>, 
   res: Response
-) => {
+) {
   try {
-    const body = req.body;
+    const { name, city, addressLine1, country } = req.body;
 
     const user = await UserService.findById(req.userId);
     if (!user) return res.status(404).send('User does not exist');
 
-    const updated = await UserService.updateUser({ _id: req.userId }, { ...body });
+    const updated = await UserService.updateUser({ _id: req.userId }, { name, city, addressLine1, country });
     if (updated) return res.status(200).send(updated.toObject());
 
-    return res.status(400).send('Failed to update user.');
+    return res.status(400).send('Failed to update user');
   } catch (error) {
-    console.log('An error occured', error);
-    return res.status(500).send(`Internal Server Error.`);
+    console.log('An error occurred:', error);
+    return res.status(500).send(`Internal Server Error`);
   }
 };
 
