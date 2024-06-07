22c7e7eb6911aeb7a5eab1fc16c533f1b4316a29
Jesse Emana <jesseemana@gmail.com>
2024-06-07 18:40:04 +0200

    modified restaurant upload functions

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
+import log from './logger';
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -6,19 +7,50 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-async function uploadImage(file: Express.Multer.File) {
+export async function uploadThumb(file: Express.Multer.File) {
   try {
     const image = file;
     const base64Image = Buffer.from(image.buffer).toString('base64');
     const dataURI = `data:${image.mimetype};base64,${base64Image}`;
-    const uploadResponse = await cloudinary.uploader.upload(dataURI);
+
+    const uploadResponse = await cloudinary.uploader.upload(dataURI, {
+      transformation: [
+        { width: 500, crop: 'thumb' },
+        { quality: 'auto' },
+        { fetch_format: 'auto' }
+      ],
+      folder: 'quickeatz',
+    });
+
     return {
       image: uploadResponse.url,
       coudinary_id: uploadResponse.public_id
     };
   } catch (error) {
-    console.log(`Error occured while uploading picture:`, error);
+   log.error(`Error uploading picture: ${error}`);
   }
 };
 
-export default uploadImage;
+export async function uploadImage(file: Express.Multer.File) {
+  try {
+    const image = file;
+    const base64Image = Buffer.from(image.buffer).toString('base64');
+    const dataURI = `data:${image.mimetype};base64,${base64Image}`;
+
+    const uploadResponse = await cloudinary.uploader.upload(dataURI, {
+      transformation: [
+        { width: 1000, crop: 'scale' },
+        { quality: 'auto' },
+        { fetch_format: 'auto' }
+      ],
+      folder: 'quickeatz',
+    });
+
+    return {
+      image: uploadResponse.url,
+      coudinary_id: uploadResponse.public_id
+    };
+  } catch (error) {
+   log.error(`Error uploading picture: ${error}`);
+  }
+};
