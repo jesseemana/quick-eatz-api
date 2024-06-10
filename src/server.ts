0b7feb4611ff8c26d69a86c357f8a42406623939
Jesse Emana <jesseemana@gmail.com>
2024-06-10 23:08:07 +0200

    added helmet

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,37 @@
 import 'dotenv/config';
 import express, { Request, Response } from 'express';
 import { 
-  UserRoute, 
-  OrderRoute, 
   RestaurantRoute, 
-  MyRestaurantRoute,
+  UserRoute, 
   FavoriteRoute, 
+  OrderRoute, 
+  ReviewRoute,
+  MyRestaurantRoute,
 } from './routes';
 import cors from 'cors';
-import database from './utils/database';
+import helmet from 'helmet';
+import { log, database } from './utils';
 import { allowedOrigins } from './constants';
-import log from './utils/logger';
 
 const app = express();
 const PORT = parseInt(process.env.PORT as string) || 3001;
 
+app.use(helmet())
 app.use(cors({ origin: [...allowedOrigins] })); 
 // TODO: create/install strip webhook stuff then add webhook command in pkg.json
 app.use('/api/order/checkout/webhook', express.raw({ type: '*/*' }));
 app.use(express.json({ limit: '5MB' }));
 
-app.get('/health', (_req: Request, res: Response) => res.status(200).json({ message: 'OK' }));
+app.get('/health', (_req: Request, res: Response) => {
+  res.status(200).json({ message: 'OK' });
+});
 
 app.use('/api/my/user', UserRoute);
 app.use('/api/restaurant', RestaurantRoute);
 app.use('/api/order', OrderRoute);
 app.use('/api/my/restaurant', MyRestaurantRoute);
 app.use('/api/favorite', FavoriteRoute);
+app.use('/api/review', ReviewRoute);
 
 function main() {
   const server = app.listen(PORT, () => {
